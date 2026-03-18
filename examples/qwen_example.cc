bcca98b1a7cf471f495590e5a55f8e9127fcf6bc
Junkui Zhang <364772080@qq.com>
2026-03-18 19:23:24 +0800

    add example

---
+++ examples/qwen_example.cc
@@ -0,0 +1,26 @@
+#include "qwen.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::cout << "=============================================" << std::endl;
+    std::cout << " Lucciola (Qwen3) Native CUDA Inference Core " << std::endl;
+    std::cout << "=============================================" << std::endl;
+
+    lucciola::QwenModel qwen("/home/zjk/cuda/lucciola/models/Qwen3-0.6B");
+
+    std::string input = "To be or not to be, that's a question.";
+
+    std::vector<int> input_ids = qwen.get_tokenizer().encode(input);
+
+    std::vector<int> out_tokens = qwen.generate(input_ids, 1);
+
+    for (int token : out_tokens) {
+        std::cout << qwen.get_tokenizer().decode(token) << std::flush;
+    }
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Engine Shutting Down." << std::endl;
+    return 0;
+}
